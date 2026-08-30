8d8f94acb90ccfa61ac7f5562e64d9c9a82b970f
1145zh9178cn67 <tjilai@zohomail.com>
2026-08-30 15:46:37 +0800

    Add files via upload
    就两个文件，分别运行就好了，里面的IP记得改成服务端的，端口也可以改，server控制client，桌面查看……（画质和帧率感人），欢迎改进！

---
+++ 一键安装库.bat
@@ -0,0 +1,11 @@
+pip install tkinter
+pip install socket
+pip install threading
+pip install json
+pip install base64
+pip install io
+pip install PIL
+pip install os
+pip install time
+pip install subprocess
+cmd
